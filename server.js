fc9cabcfff860983717c747af82251d4b3387af9
agarwalomkar1 <agarwalomkar16@gmail.com>
2026-05-22 09:48:42 +0530

    Fix Vercel Express entrypoint detection

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import express from "express";
+
 import app from "./server/app.js";
+
+// Vercel's Express detector expects the root entrypoint to import express directly.
+void express;
 
 export default app;
